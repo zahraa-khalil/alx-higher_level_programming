2f1cf6e471abb25448161b39f433e934749b9e68
zahraa-khalil <zahraa.khalil2020@gmail.com>
2023-11-30 16:59:24 +0200

    handle dynamic script

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == '__main__':
+    import sys
+    name = sys.argv[0]
+    args = sys.argv[1:]
+
+print("{} argument{}{}".format(len(args), "s" if len(
+    args) != 1 else "", ":" if len(args) > 0 else "."))
+if len(args) > 0:
+    index = 1
+    for arg in args:
+        print("{}:{} ".format(index, arg))
+        index += 1

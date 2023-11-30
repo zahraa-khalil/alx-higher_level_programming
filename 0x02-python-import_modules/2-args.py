74155456608235ce7a1fd0909d8f46cc172500e0
zahraa-khalil <zahraa.khalil2020@gmail.com>
2023-11-30 17:09:35 +0200

    handle dynamic script

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,11 +3,10 @@ if __name__ == '__main__':
     import sys
     name = sys.argv[0]
     args = sys.argv[1:]
-
-print("{} argument{}{}".format(len(args), "s" if len(
+    print("{} argument{}{}".format(len(args), "s" if len(
     args) != 1 else "", ":" if len(args) > 0 else "."))
-if len(args) > 0:
-    index = 1
-    for arg in args:
-        print("{}: {} ".format(index, arg))
-        index += 1
+    if len(args) > 0:
+        index = 1
+        for arg in args:
+            print("{}: {} ".format(index, arg))
+            index += 1

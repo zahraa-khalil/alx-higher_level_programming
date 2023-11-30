c1652ed6de0bcc44b4ccae473171b91835c14a67
zahraa-khalil <zahraa.khalil2020@gmail.com>
2023-12-01 01:03:20 +0200

    rgv

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
+
 if __name__ == '__main__':
     import sys
     argv = sys.argv[1:]
     print("{} argument{}{}".format(len(argv), "s" if len(
-    argv) != 1 else "", ":" if len(argv) > 0 else "."))
+        argv) != 1 else "", ":" if len(argv) > 0 else "."))
     if len(argv) > 0:
         index = 1
         for arg in argv:

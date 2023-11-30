091f8c015e51415ea58c7591fcd774472ae04ea6
zahraa-khalil <zahraa.khalil2020@gmail.com>
2023-11-30 16:08:58 +0200

    __name__

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-
-import add_0 
-a = 1
-b = 2
-result = add_0.add(1, 2)
-print("{} + {} = {}".format(a, b, result))
+if __name__ == '__main__':
+    from add_0 import add
+    a = 1
+    b = 2
+    result = add(1, 2)
+    print("{} + {} = {}".format(a, b, result))

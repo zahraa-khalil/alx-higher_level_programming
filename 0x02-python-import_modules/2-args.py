c08a61988fcac9b8edcfa93a5daf32fe62363003
zahraa-khalil <zahraa.khalil2020@gmail.com>
2023-12-01 01:02:42 +0200

    rgv

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 if __name__ == '__main__':
     import sys
-    name = sys.argv[0]
     argv = sys.argv[1:]
     print("{} argument{}{}".format(len(argv), "s" if len(
     argv) != 1 else "", ":" if len(argv) > 0 else "."))

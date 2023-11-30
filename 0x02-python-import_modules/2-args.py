557f6c28c837a1a9e7fe7350e357a1acba0e0994
zahraa-khalil <zahraa.khalil2020@gmail.com>
2023-12-01 01:08:01 +0200

    rgv

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,30 @@
 #!/usr/bin/python3
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     import sys
-    argv = sys.argv[1:]
-    print("{} argument{}{}".format(len(argv), "s" if len(
-        argv) != 1 else "", ":" if len(argv) > 0 else "."))
-    if len(argv) > 0:
-        index = 1
-        for arg in argv:
-            print("{}: {} ".format(index, arg))
-            index += 1
+    i = len(sys.argv) - 1
+
+    if i == 0:
+        print("{} arguments.".format(i))
+    elif i == 1:
+        print("{} argument:".format(i))
+    else:
+        print("{} arguments:".format(i))
+
+    if i >= 1:
+        i = 0
+        for arg in sys.argv:
+            if i != 0:
+                print("{}: {}".format(i, arg))
+            i += 1
+
+
+# if __name__ == '__main__':
+#     import sys
+#     argv = sys.argv[1:]
+#     print("{} argument{}{}".format(len(argv), "s" if len(
+#         argv) != 1 else "", ":" if len(argv) > 0 else "."))
+#     if len(argv) > 0:
+#         index = 1
+#         for arg in argv:
+#             print("{}: {} ".format(index, arg))
+#             index += 1

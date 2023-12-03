bce9c39da0c79298a542603ef5715748789edc44
zahraa-khalil <zahraa.khalil2020@gmail.com>
2023-12-04 00:09:16 +0200

    update

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,7 +2,6 @@
 
 def element_at(my_list, idx):
     if idx < 0:
-        return "Nowwwne"
+        return "None"
     elif idx > len(my_list) + 1:
-         return "Noneee"
-    
+        return "None"

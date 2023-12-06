8a971320c120026287e1a072ed21348bd3910bbf
zahraa-khalil <zahraa.khalil2020@gmail.com>
2023-12-07 00:33:10 +0200

    dictionary

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-# a function that returns a set of all elements present 
-#in only one set.
+# a function that returns a set of all elements present
+# in only one set.
 # set difference
 
 def only_diff_elements(set_1, set_2):

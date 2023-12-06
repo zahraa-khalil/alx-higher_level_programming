618e073b2c2e2e20535c2a7cbee458eefc942164
zahraa-khalil <zahraa.khalil2020@gmail.com>
2023-12-07 00:43:35 +0200

    sort

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -4,11 +4,5 @@
 # set difference
 
 def print_sorted_dictionary(a_dictionary):
-    return dict(sorted(a_dictionary.items()))
-
-
-
-print_sorted_dictionary = __import__('6-print_sorted_dictionary').print_sorted_dictionary
-
-a_dictionary = { 'language': "C", 'Number': 89, 'track': "Low level", 'ids': [1, 2, 3] }
-print_sorted_dictionary(a_dictionary)
+    sorted_dictionary = dict(sorted(a_dictionary.items()))
+    print(sorted_dictionary)

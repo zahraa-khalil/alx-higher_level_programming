fb9f350b70156791006e5b50e139dfaa966e6c3b
zahraa-khalil <zahraa.khalil2020@gmail.com>
2024-01-06 15:57:03 +0200

    task-1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Square Module"""
+
+
+class Square:
+    """Define a square"""
+
+    def __init__(self, size=None):
+        """Constructor
+            args: size
+        """
+        self.__size = size

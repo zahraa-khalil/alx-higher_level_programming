6dbc7c97b4a77e87794131be1bbd2cd8a92f982a
zahraa-khalil <zahraa.khalil2020@gmail.com>
2024-01-07 18:44:19 +0200

    task4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -9,24 +9,27 @@ class Square():
     def __init__(self, size=0):
         """Construct
         Args: size
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """this getter method for square size."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """this setter method for square size.
         Raise:
             TypeError:size must be an integer
             ValueError:size must be >= 0
         """
 
-        def size(self):
-            """this getter method for square size."""
-            return self.__size
-
-        def size(self, value):
-            """this setter method for square size."""
-            self.__size = value
-
-        if not isinstance(size, int):
+        if not isinstance(value, int):
             raise TypeError('size must be an integer')
-        if size < 0:
+        if value < 0:
             raise ValueError('size must be >= 0')
-        self.__size = size
+        self.__size = value
 
     def area(self):
         """Return: current square area"""

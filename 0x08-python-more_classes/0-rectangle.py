f91f8a267bf6a9be877f4b253304f14049bdc44a
Fatma Khalil <folla@Fatmas-MacBook-Pro.local>
2024-04-13 16:23:54 +0200

    update

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Rectangle Module"""
+
+
+class Rectangle():
+    """Rectangle class
+    Args: None
+    Returns: None
+    """
+    pass
---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Rectangle Module"""
+
+
+class Rectangle():
+    """Rectangle class
+    Args: 
+    width: width of the rectangle
+    height: height of the rectangle
+    Returns: None
+    """
+    

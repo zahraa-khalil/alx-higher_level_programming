16503d52b71d78a998021b3e4555e2a0d3553609
Fatma Khalil <folla@192.168.1.9>
2024-04-17 01:30:55 +0200

    update8

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
+"""defining readfile function"""
 def read_file(filename=""):
-    """ function that reads a text file (UTF8) and prints it to stdout """
+    """function that reads a text file (UTF8)"""
 
     with open(filename, encoding='utf-8') as f:
         print(f.read(), end="")

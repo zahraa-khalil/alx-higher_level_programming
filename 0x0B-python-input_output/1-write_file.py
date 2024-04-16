91be431464d11b0efa6e7fd0bb8e78815f43650a
Fatma Khalil <folla@192.168.1.9>
2024-04-17 01:43:50 +0200

    update

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -7,4 +7,4 @@ def write_file(filename="", text=""):
     and returns the number of characters written
     """
     with open(filename, 'w',  encoding='utf-8') as f:
-        f.write(text)
+        return f.write(text)

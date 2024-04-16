85857d1c4204d4a19043c70e5a6ce63e62300681
Fatma Khalil <folla@192.168.1.9>
2024-04-17 01:01:35 +0200

    update

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
 def read_file(filename=""):
-    with open(filename , "r") as f:
+    with open(filename , "r", encoding="utf-8") as f:
         content = f.read()
         print(content)

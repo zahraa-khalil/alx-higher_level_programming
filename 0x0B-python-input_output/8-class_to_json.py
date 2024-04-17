f0e23e880d01682a228bd42a31b7e303c5a166d4
Fatma Khalil <folla@192.168.1.9>
2024-04-17 03:07:35 +0200

    update

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -2,9 +2,6 @@
 """defining function"""
 
 
-import json
-
-
 def class_to_json(obj):
     """ function that returns the dictionary description
     with simple data structure for JSON serialization of an object:

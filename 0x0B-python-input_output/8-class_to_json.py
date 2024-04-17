fff76a8af8d03bffa7a74f0917916fc1962d6f84
Fatma Khalil <folla@192.168.1.9>
2024-04-17 03:08:49 +0200

    update

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -6,4 +6,5 @@ def class_to_json(obj):
     """ function that returns the dictionary description
     with simple data structure for JSON serialization of an object:
     """
-    return {attr: getattr(obj, attr) for attr in obj.__dict__ if isinstance(getattr(obj, attr), (list, dict, str, int, bool))}
+    return {attr: getattr(obj, attr) for attr in obj.__dict__ if
+            isinstance(getattr(obj, attr), (list, dict, str, int, bool))}

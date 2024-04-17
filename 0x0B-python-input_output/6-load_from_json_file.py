c9dd218e99a89eed67c5588ccd3e4fc5eb22f1c0
Fatma Khalil <folla@192.168.1.9>
2024-04-17 02:18:40 +0200

    update

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""defining function"""
+
+
+import json
+
+
+def load_from_json_file(filename):
+    """function that creates an Object from a “JSON file"""
+    with open(filename, 'r',  encoding='utf-8') as f:
+        return json.load(f)

625e1fe7a9d3369c8b30ff074ce5d1b8368eb33d
Fatma Khalil <folla@192.168.1.9>
2024-04-17 02:11:02 +0200

    update

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
+"""defining function"""
+
+
 import json
 
 
 def to_json_string(my_obj):
-    """function that returns the JSON representation
-    of an object (string)
+    """function returns JSON representation
+    of an object
     """
     return json.dumps(my_obj)

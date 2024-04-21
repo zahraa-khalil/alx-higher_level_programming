14a6aa9f8d39120638188eb52350dd38e945a1f3
Fatma Khalil <folla@192.168.1.9>
2024-04-21 19:03:44 +0200

    update

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Base Module"""
+
+
+class Base:
+    """Base class: base of all other classes in this project"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Constructor
+        Args:
+            id: id of object
+        """
+
+        if id is not None:
+            self.id = id
+        else:
+            self.id = Base.__nb_objects
+            Base.__nb_objects += 1
+
+    def __repr__(self):
+        return f"Base(id={self.id})"

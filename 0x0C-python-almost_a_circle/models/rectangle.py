2a6eb0e8606eca401f9536fb441a38acf8425642
Fatma Khalil <folla@192.168.1.9>
2024-04-22 23:55:39 +0200

    update

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -97,11 +97,11 @@ class Rectangle(Base):
         # Check the number of arguments and assign accordingly
         if len(args) > 0:
             self.id = args[0]
-        if len(args) > 1:
+        if len(args) >= 2:
             self.width = args[1]
-        if len(args) > 2:
+        if len(args) >= 3:
             self.height = args[2]
-        if len(args) > 3:
+        if len(args) >= 4:
             self.x = args[3]
-        if len(args) > 4:
+        if len(args) >= 5:
             self.y = args[4]

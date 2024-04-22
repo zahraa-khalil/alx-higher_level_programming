ecf79e22d13d70c4b2f7f10b0d25f6ff1768bdbb
Fatma Khalil <folla@192.168.1.9>
2024-04-23 01:16:51 +0200

    update

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -48,7 +48,11 @@ class Square(Rectangle):
             for key, value in kwargs.items():
                 setattr(self, key, value)
 
-
+    def to_dictionary(self):
+        """Return the dictionary representation of a Rectangle"""
+        return {'id': self.id, 'size': self.width,
+                'x': self.x, 'y': self.y}
+    
     def __str__(self):
         """Returns a string representation of the square."""
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"

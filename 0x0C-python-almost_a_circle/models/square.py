f8c3e4de59438658b1b53cb4a866df8cd1c6c150
Fatma Khalil <folla@192.168.1.9>
2024-04-23 01:08:47 +0200

    update

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -33,6 +33,22 @@ class Square(Rectangle):
         self.width = value
         self.height = value
 
+
+    def update(self, *args, **kwargs):
+        """ Check the number of arguments and assign accordingly"""
+        if args:
+            if len(args) > 0:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.size = args[1]
+            if len(args) >= 3:
+                self.x = args[2]
+            if len(args) >= 4:
+                self.y = args[3]
+        elif kwargs:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
+        
     def __str__(self):
         """Returns a string representation of the square."""
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"

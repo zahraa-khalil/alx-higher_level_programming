75cfe97921564bf28aff5b2d1d31e34f632220bb
Fatma Khalil <folla@192.168.1.9>
2024-04-23 02:19:44 +0200

    update

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
+import os
 import unittest
+import json
 
 from models.base import Base
 
@@ -32,6 +34,30 @@ class TestBase(unittest.TestCase):
         self.assertEqual(base5.id, 20, "Explicit ID for base5 should be 20")
         self.assertEqual(
             base6.id, 4, "Auto-incremented ID for base6 should be 4 after an explicit ID assignment")
+    def test_manual_id_assignment(self):
+        """Test that manual IDs are assigned correctly."""
+        base3 = Base(10)
+        self.assertEqual(base3.id, 10)
+        
+    def test_to_json_string(self):
+        """Test JSON string conversion."""
+        dict_list = [{"id": 1}, {"id": 2}]
+        json_str = Base.to_json_string(dict_list)
+        self.assertEqual(json_str, json.dumps(dict_list))
+        self.assertEqual(Base.to_json_string(None), "[]")
+        self.assertEqual(Base.to_json_string([]), "[]")
+
+    def test_save_to_file_and_load(self):
+        """Test saving to a file and loading from it."""
+        base1 = Base(1)
+        base2 = Base(2)
+        # Base.save_to_file([base1, base2])
+        # list_loaded = Base.load_from_file()
+        # self.assertIsInstance(list_loaded, list)
+        # self.assertEqual(len(list_loaded), 2)
+        
+        # os.remove("Base.json")
+
 
 
 if __name__ == '__main__':

b5961259e55e8c23fc023f7c15c19ad67372105b
Fatma Khalil <folla@192.168.1.13>
2024-05-06 15:47:43 +0300

    update

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -57,9 +57,12 @@ class Rectangle:
         return self.build_representation()
 
     def build_representation(self):
-        """Print in stdout the Rectangle with the character #.
-        if size is 0: print empty line
+        """build_representation in stdout the Rectangle with 
+        the character #. if size is 0: print empty line
         """
         if self.width == 0 or self.height == 0:
             return ""
         return "\n".join("#" * self.width for _ in range(self.height))
+
+    def __repr__(self):
+        return "Rectangle({0}, {1})".format(self.width, self.height)

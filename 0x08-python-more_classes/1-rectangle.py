b061b05e8791f1f86011ba3e23e146f5d407843c
Fatma Khalil <folla@192.168.1.13>
2024-05-06 14:44:55 +0300

    update

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -11,13 +11,13 @@ class Rectangle:
     """
 
     def __init__(self, width=0, height=0):
-        self._width = width
-        self._height = height
+        self.__width = width
+        self.__height = height
 
     @property
     def width(self):
         """width getter"""
-        return self._width
+        return self.__width
 
     @width.setter
     def width(self, value):
@@ -26,12 +26,12 @@ class Rectangle:
             raise TypeError('width must be an integer')
         if value < 0:
             raise ValueError('width must be >= 0')
-        self._width = value
+        self.__width = value
 
     @property
     def height(self):
         """height getter"""
-        return self._height
+        return self.__height
 
     @height.setter
     def height(self, value):
@@ -40,4 +40,4 @@ class Rectangle:
             raise TypeError('height must be an integer')
         if value < 0:
             raise ValueError('height must be >= 0')
-        self._height = value
+        self.__height = value

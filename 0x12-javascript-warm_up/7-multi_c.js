8d5674459853cca64c2adc3011c79b4f303b5d4b
zahraa-khalil <zahraa.khalil2020@gmail.com>
2024-06-19 02:46:18 +0300

    update

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
 /* print first argument converted in integer */
 
-let num = parseInt(process.argv[2])
+const num = parseInt(process.argv[2]);
 if (isNaN(num)) {
-    console.log('Missing number of occurrences');
-}
-else{
-    for (let i = 0; i < num; i++){
-        console.log('C is fun');
-    }
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < num; i++) {
+    console.log('C is fun');
+  }
 }

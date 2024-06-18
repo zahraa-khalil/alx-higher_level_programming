0f41bbda7ec5e1cd5161872bfcbd5c5d54f451e6
zahraa-khalil <zahraa.khalil2020@gmail.com>
2024-06-19 02:41:46 +0300

    update

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 /* print first argument converted in integer */
 
-let arr =  ['C is fun', 'Python is cool','JavaScript is amazing']
-for (let i = 0; i < arr.length; i++){
-    console.log(arr[i]);
+const arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let i = 0; i < arr.length; i++) {
+  console.log(arr[i]);
 }

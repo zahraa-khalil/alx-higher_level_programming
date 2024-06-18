f4046ff274e3266a0590b039a582d14a131ef4c8
zahraa-khalil <zahraa.khalil2020@gmail.com>
2024-06-19 02:39:22 +0300

    update

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,11 +1,9 @@
 #!/usr/bin/node
 /* print first argument converted in integer */
 
-let num = parseInt(process.argv[2])
-
+const num = parseInt(process.argv[2]);
 if (isNaN(num)) {
-    console.log('Not a number');
-
-} else{
-    console.log(`My number: ${num}`);
+  console.log('Not a number');
+} else {
+  console.log(`My number: ${num}`);
 }

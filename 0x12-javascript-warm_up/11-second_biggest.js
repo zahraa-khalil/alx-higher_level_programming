f40b7a32d9921ed9afdf784762e0a28269e24b75
zahraa-khalil <zahraa.khalil2020@gmail.com>
2024-06-19 10:20:41 +0300

    handle-secondLargestNum

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,11 +1,11 @@
-#!/usr/bin/node
+#!/usr/bin/env node
 /* script that searches the second biggest integer in the list */
 
 let largetstNum = process.argv[2];
 let secondLargestNum = process.argv[3];
 
 if (process.argv.length <= 3) {
-  console.log(0);
+  console.log('0');
 } else {
   for (let i = 2; i < process.argv.length; i++) {
     if (largetstNum < process.argv[i]) {

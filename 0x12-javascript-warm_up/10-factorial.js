93feb92ccb6e87d068411b14d4f9e711efe07e46
zahraa-khalil <zahraa.khalil2020@gmail.com>
2024-06-19 12:00:38 +0300

    update-10

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const factorialNum = parseInt(process.argv[2]);
+
+function factorial(num) {
+  if (isNaN(num) || num <= 0) {
+    return 1;
+  } else {
+    const result = num * factorial(num - 1);
+    return result;
+  }
+}
+
+console.log(factorial(factorialNum));

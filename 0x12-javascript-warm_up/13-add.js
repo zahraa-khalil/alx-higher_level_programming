1a5bf3528425d11b14a94b9b0cf11652fc36e7ab
zahraa-khalil <zahraa.khalil2020@gmail.com>
2024-06-19 10:45:25 +0300

    update-13.2

--- 0x12-javascript-warm_up/13-add.js
+++ 0x12-javascript-warm_up/13-add.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 /* function that adds two integers and make it visible from outside */
 function add (a, b) {
-  console.log(a + b);
-}
+  return a + b;
+};
 
 module.exports = add;

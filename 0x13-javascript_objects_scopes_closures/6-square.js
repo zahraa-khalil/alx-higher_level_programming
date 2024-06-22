7e015572e543bdabb7457c64df8a8a9bc639aac9
zahraa-khalil <zahraa.khalil2020@gmail.com>
2024-06-22 19:17:40 +0300

    task6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const SquareBase = require('./5-square');
+
+module.exports = class Square extends SquareBase {
+  charPrint (c) {
+    if (c) {
+      for (let i = 0; i < this.height; i++) {
+        console.log(c.repeat(this.width));
+      }
+    } else {
+      for (let i = 0; i < this.height; i++) {
+        console.log('X'.repeat(this.width));
+      }
+    }
+  }
+};

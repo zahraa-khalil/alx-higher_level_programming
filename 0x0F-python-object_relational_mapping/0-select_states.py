1cc4c5757594a94c83a0faf11f12403d45c1f6f3
zahraa-khalil <zahraa.khalil2020@gmail.com>
2024-06-24 19:12:28 +0300

    task1.4

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,34 +1,22 @@
 #!/usr/bin/python3
-"""List from table state"""
-import MySQLdb
-import sys
+"""Lists states"""
 
-"""Replace these values with your actual database credentials"""
-mysql_username = sys.argv[1]
-mysql_password = sys.argv[2]
-database_name = sys.argv[3]
+import MySQLdb
+from sys import argv
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(
+    conn = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user=mysql_username,
-        passwd=mysql_password,
-        db=database_name,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3],
         charset="utf8",
     )
-
-    """ Create a cursor object to interact with the database"""
-    cursor = db.cursor()
-
-    """ Execute a query"""
-    cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
-
-    rows = cursor.fetchall()
-
-    for row in rows:
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
         print(row)
-
-    """ Close the cursor and connection """
-    cursor.close()
-    db.close()
+    cur.close()
+    conn.close()

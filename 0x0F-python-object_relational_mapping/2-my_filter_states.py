4bc7f0584d1c5b8c23483149456c184d31c054c4
zahraa-khalil <zahraa.khalil2020@gmail.com>
2024-06-27 09:13:12 +0300

    filterByState

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""List from table state"""
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    mysql_username = argv[1]
+    mysql_password = argv[2]
+    database_name = argv[3]
+    argument = argv[4]
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=mysql_username,
+        passwd=mysql_password,
+        db=database_name,
+        charset="utf8"
+    )
+    cursor = db.cursor()
+    query = "SELECT * FROM states WHERE BINARY name LIKE %s "
+    "ORDER BY states.id ASC"
+    cursor.execute(query, (argument + '%',))
+    rows = cursor.fetchall()
+    for row in rows:
+        # Decode and cast each column appropriately
+        decoded_row = (
+            int(row[0]),
+            row[1].decode('utf-8') if isinstance(row[1], bytes) else row[1]
+        )
+        print(decoded_row)
+    cursor.close()
+    db.close()

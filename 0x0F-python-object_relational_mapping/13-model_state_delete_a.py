aea83045bc9d5c68be5adcaa884ab1b5004b9ef3
zahraa-khalil <zahraa.khalil2020@gmail.com>
2024-06-29 10:25:17 +0300

    task13

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""List first state"""
+import sys
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import (create_engine)
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2],
+                                   sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    letter = 'a'
+    states = session.query(State).filter(State.name.like(f'%{letter}%')).all()
+    if states:
+        for state in states:
+            session.delete(state)
+            session.commit()
+
+    session.close()

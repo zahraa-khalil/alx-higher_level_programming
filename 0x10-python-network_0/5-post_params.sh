5db730e677729161d93b53f94e4e1485ecbca064
zahraa-khalil <zahraa.khalil2020@gmail.com>
2024-07-04 16:00:20 +0300

    update

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Bash script displays the body of the response
-curl -sX POST -H "email: test@gmail.com" -H "subject: I will always be here for PLD" "$1" -L
+curl -sX POST -d "email: test@gmail.com&subject: I will always be here for PLD" "$1" -L

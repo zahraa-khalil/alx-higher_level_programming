c7df4d9910c23e3449f8a57e513fc5978933b206
zahraa-khalil <zahraa.khalil2020@gmail.com>
2024-07-04 16:36:59 +0300

    update

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,6 +4,5 @@
 
 def find_peak(list_of_integers):
     """Finds the peak of list of integers"""
-    list_of_integers.sort()
-    print(list_of_integers[-1])
-    # return list_of_integers[-1]
+    peak = max(list_of_integers)
+    print(peak)

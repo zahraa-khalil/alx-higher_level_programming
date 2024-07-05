8a7cfe374ad2a3ca27004f20aae57edc72a83353
zahraa-khalil <zahraa.khalil2020@gmail.com>
2024-07-05 04:17:05 +0300

    update

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,5 +1,17 @@
 #!/usr/bin/python3
-""" finding peak in list """
+"""script for finding peak in list of ints, interview prep
+"""
+
+"""
+    THOUGHT PROCESS
+        it is not sorted, so sorting would take n(log(n))
+            -> not worth sorting
+        looping through and keeping track of max (brute force)
+            -> O(n)
+
+        possibly looping from each end reducing to 1/2 run time
+            -> still O(n)
+"""
 
 
 def find_peak(list_of_integers):

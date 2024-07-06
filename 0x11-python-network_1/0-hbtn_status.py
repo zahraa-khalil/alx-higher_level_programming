d67c21a544e743dfdc06967d77a3581a2f6d55d2
zahraa-khalil <zahraa.khalil2020@gmail.com>
2024-07-06 22:22:37 +0300

    update.1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,6 +7,6 @@ if __name__ == "__main__":
     with urllib.request.urlopen(url) as response:
         html = response.read()
         print(f"Body response:")
-        print(f"   - type:  {type(html)}")
-        print(f"   - content: {html}")
-        print(f"   - utf8 content: {html.decode('utf-8')}")
+        print(f"\t- type: {type(html)}")
+        print(f"\t- content: {html}")
+        print(f"\t- utf8 content: {html.decode('utf-8')}")

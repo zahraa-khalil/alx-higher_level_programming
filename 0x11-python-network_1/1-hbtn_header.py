6f969150f39340d4bc7346049909fc60c8ecb14d
zahraa-khalil <zahraa.khalil2020@gmail.com>
2024-07-07 14:42:11 +0300

    update

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""script to fetch data from URL"""
+import urllib.request
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    with urllib.request.urlopen(url) as response:
+        headers = response.headers
+        id = headers.get('X-Request-Id')  
+        print(id)

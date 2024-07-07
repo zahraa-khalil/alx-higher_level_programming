cdbccd849a91afc7daecd5b60614ac366d7373ad
zahraa-khalil <zahraa.khalil2020@gmail.com>
2024-07-07 15:53:34 +0300

    update

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -7,5 +7,6 @@ if __name__ == "__main__":
     url = sys.argv[1]
     with urllib.request.urlopen(url) as response:
         headers = response.headers
+        print(f"Response headers:{response}")
         id = headers.get('X-Request-Id')
         print(id)
--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -10,4 +10,4 @@ if __name__ == "__main__":
             res = response.read().decode('utf-8')
             print(res)
     except urllib.error.HTTPError as e:
-        print(f"Error: {e.code}")
+        print(f"Error code: {e.code}")

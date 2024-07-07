1613a5f72026cd0b02f253d850435f29eea27629
zahraa-khalil <zahraa.khalil2020@gmail.com>
2024-07-07 15:39:49 +0300

    update

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""script to fetch data from URL and handle error"""
+import urllib.request
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    try:
+        with urllib.request.urlopen(url) as response:
+            res = response.read().decode('utf-8')
+            print(res)
+    except urllib.error.HTTPError as e:
+        print(f"Error: {e.reason}")

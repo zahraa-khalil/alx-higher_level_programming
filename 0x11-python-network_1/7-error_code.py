b72f0ec42f99676ddd04d43aa9dffba71b06dae3
zahraa-khalil <zahraa.khalil2020@gmail.com>
2024-07-07 17:19:06 +0300

    update

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""script to fetch data from URL"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    res = requests.post(url)
+
+    if res.status_code >= 400:
+        print(f"Error code: {res.status_code}")
+    else:
+        print(res.text)

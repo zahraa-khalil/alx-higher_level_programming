2ad0b6566da064914b9cb7e145d891d8ff266689
zahraa-khalil <zahraa.khalil2020@gmail.com>
2024-07-07 17:32:37 +0300

    update

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -5,8 +5,16 @@ import sys
 
 
 if __name__ == "__main__":
-    url = sys.argv[1]
-    letter = sys.argv[2]
-    emailObj = {'letter': letter}
-    res = requests.post(url, emailObj)
-    print(res.text)
+    url = 'http://0.0.0.0:5000/search_user'
+    q = sys.argv[1] if len(sys.argv) == 2 else ""
+    letterObj = {'q': q}
+    res = requests.post(url, letterObj)
+    try:
+        r_dict = res.json()
+        id, name = r_dict['id'], r_dict.get('name')
+        if len(r_dict) == 0 or not id or not name:
+            print("No result")
+        else:
+            print("[{}] {}".format(r_dict.get('id'), r_dict.get('name')))
+    except Exception:
+        print("Not a valid JSON")

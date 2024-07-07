bea38c5a19b94ceac8ae07d85a3cb87652469543
zahraa-khalil <zahraa.khalil2020@gmail.com>
2024-07-07 14:43:04 +0300

    task1.1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -7,5 +7,5 @@ if __name__ == "__main__":
     url = sys.argv[1]
     with urllib.request.urlopen(url) as response:
         headers = response.headers
-        id = headers.get('X-Request-Id')  
+        id = headers.get('X-Request-Id')
         print(id)

a6475a636961b0b47bbc68f70a695d68ef359c97
zahraa-khalil <zahraa.khalil2020@gmail.com>
2024-07-08 16:16:46 +0300

    update

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -7,8 +7,8 @@ import sys
 if __name__ == "__main__":
     url = 'http://0.0.0.0:5000/search_user'
     username = sys.argv[1]
-    token = sys.argv[2]
-    response = requests.get(url, auth=(username, token))
+    password = sys.argv[2]
+    response = requests.get(url, auth=(username, password))
 
     if response.status_code == 200:
         print(response.json()['id'])

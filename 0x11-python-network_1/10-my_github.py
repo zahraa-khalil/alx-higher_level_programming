19f1a3a5c21ccec6acb5a1ee11d5586c4d75a9dc
zahraa-khalil <zahraa.khalil2020@gmail.com>
2024-07-08 16:26:18 +0300

    update

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -6,12 +6,12 @@ import sys
 
 
 if __name__ == "__main__":
-    url = 'http://0.0.0.0:5000/search_user'
+    url = 'https://api.github.com/user'
     username = sys.argv[1]
     password = sys.argv[2]
     response = requests.get(url, auth=HTTPBasicAuth(username, password))
-    print(response.json().get('id'))
-    # if response.status_code == 200:
-    #     print(response.json().get('id'))
-    # else:
-    #     print(None)
+
+    if response.status_code == 200:
+        print(response.json().get('id'))
+    else:
+        print(None)

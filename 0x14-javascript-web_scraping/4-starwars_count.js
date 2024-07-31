f74e34378a6b4643d84bbe9aeaaf9e1a54e1bc88
zahraa-khalil <zahraa.khalil2020@gmail.com>
2024-07-31 12:45:12 +0300

    update

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -3,15 +3,19 @@ const request = require('request');
 
 const URL = process.argv[2];
 const character = 'https://swapi-api.alx-tools.com/api/people/18/';
-let charactersArray = [];
+let count = 0;
 
 request(URL, { json: true }, (err, res, body) => {
   if (err) { return console.log(err); }
-  const data = body.results;
-  data.map(x => {
-    const result = x.characters.filter(link => link === character);
-    charactersArray = charactersArray.concat(result);
-    return charactersArray.length;
+
+  if (!body.results || !Array.isArray(body.results)) {
+    return console.log('Invalid or empty results.');
+  }
+
+  body.results.forEach(item => {
+    if (item.characters.includes(character)) {
+      count++;
+    }
   });
-  console.log(charactersArray.length);
+  console.log(count);
 });

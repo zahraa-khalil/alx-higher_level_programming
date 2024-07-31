f220b41d53cfe50fbc5db9d03a6eae5abd39908e
zahraa-khalil <zahraa.khalil2020@gmail.com>
2024-07-31 11:52:41 +0300

    update

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -4,6 +4,6 @@ const fs = require('fs');
 const fileName = process.argv[2];
 const writeData = process.argv[3];
 
-fs.writeFile(fileName, writeData,'utf8', (err, data) => {
+fs.writeFile(fileName, writeData, 'utf8', (err) => {
   if (err) throw err;
 });
